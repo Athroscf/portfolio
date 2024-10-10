d3919ef356e05c59416a060c47b49e447fe38e9f
Christopher Fiallos <fialloschris1@gmail.com>
2024-10-10 11:36:12 -0600

    feat: added effects and styles to config in resources.

--- src/app/resources/config.js
+++ src/app/resources/config.js
@@ -3,6 +3,12 @@ const display = {
   time: true,
 };
 
+const effects = {
+  gradient: true,
+  dots: true,
+  lines: false,
+};
+
 const routes = {
   "/": true,
   "/about": true,
@@ -11,4 +17,16 @@ const routes = {
   "/gallery": true,
 };
 
-export { display, routes };
+const style = {
+  theme: "dark", // dark | light
+  neutral: "slate", // sand | gray | slate
+  brand: "green", // blue | indigo | violet | magenta | pink | red | orange | yellow | moss | green | emerald | aqua | cyan
+  accent: "indigo", // blue | indigo | violet | magenta | pink | red | orange | yellow | moss | green | emerald | aqua | cyan
+  solid: "contrast", // color | contrast
+  solidStyle: "flat", // flat | plastic
+  border: "playful", // rounded | playful | conservative
+  surface: "filled", // filled | translucent
+  transition: "all", // all | micro | macro
+};
+
+export { display, effects, routes, style };
